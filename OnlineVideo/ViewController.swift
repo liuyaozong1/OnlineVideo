76f706d5234d4a80098d24914789ed29287cec58
刘耀宗 <648731281@qq.com>
2021-06-16 09:30:42 +0800

    Initial Commit

---
+++ OnlineVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineVideo
+//
+//  Created by 刘耀宗 on 2021/6/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
